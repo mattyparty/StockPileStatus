9cef652d8218d6c3812f43dffb6a5b1bcb418956
Israel Guillermo <israelguillermo22@gmail.com>
2020-12-22 23:17:28 -0600

    adding API route as a comment for now

--- public/js/tabulator.js
+++ public/js/tabulator.js
@@ -1,53 +1,53 @@
 // require sql
 
 const getData = () => {
-  $.get('/api/members').then((response) => {
+  $.get("/api/members").then((response) => {
     console.log(response);
     makeTable(response.results);
   });
 };
 
 const makeTable = (data) => {
-  new Tabulator('#po-table', {
+  new Tabulator("#po-table", {
     // height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
     data: data, // assign data to table
-    layout: 'fitColumns', // fit columns to width of table (optional)
+    layout: "fitColumns", // fit columns to width of table (optional)
     columns: [
       // Define Table Columns
-      { title: 'Item', field: 'item', width: 150 },
+      { title: "Item", field: "item", width: 150 },
       {
-        title: 'Supplier Number',
-        field: 'supplier_number',
-        hozAlign: 'left'
+        title: "Supplier Number",
+        field: "supplier_number",
+        hozAlign: "left",
       },
 
       {
-        title: 'Po Number',
-        field: 'po_number',
-        hozAlign: 'center'
+        title: "Po Number",
+        field: "po_number",
+        hozAlign: "center",
       },
       {
-        title: 'Po Due Date',
-        field: 'po_due_date',
-        hozAlign: 'center'
+        title: "Po Due Date",
+        field: "po_due_date",
+        hozAlign: "center",
       },
       {
-        title: 'Estimated Ship Date',
-        field: 'est_ship_date',
-        hozAlign: 'center'
+        title: "Estimated Ship Date",
+        field: "est_ship_date",
+        hozAlign: "center",
       },
       {
-        title: 'Tracking Number',
-        field: 'tracking_number',
-        hozAlign: 'center',
-        editor: 'textarea',
+        title: "Tracking Number",
+        field: "tracking_number",
+        hozAlign: "center",
+        editor: "textarea",
         editorParams: {
           elementAttributes: {
-            maxlength: '100' //set the maximum character length of the textarea element to 10 characters
-          }
-        }
-      }
-    ]
+            maxlength: "100", //set the maximum character length of the textarea element to 10 characters
+          },
+        },
+      },
+    ],
   });
 };
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,26 +1,26 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // Requiring our models and passport as we've configured it
-const db = require('../../models');
-const passport = require('../../config/passport');
-const isAuthenticated = require('../../config/middleware/isAuthenticated');
+const db = require("../../models");
+const passport = require("../../config/passport");
+const isAuthenticated = require("../../config/middleware/isAuthenticated");
 // Using the passport.authenticate middleware with our local strategy.
 // If the user has valid login credentials, send them to the members page.
 // Otherwise the user will be sent an error
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post("/login", passport.authenticate("local"), (req, res) => {
   // Sending back a password, even a hashed password, isn't a good idea
   res.json({
     email: req.user.email,
-    id: req.user.id
+    id: req.user.id,
   });
 });
 
 // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
 // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 // otherwise send back an error
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   db.User.create(req.body)
     .then(() => {
-      res.redirect(307, '/api/login');
+      res.redirect(307, "/api/login");
     })
     .catch((err) => {
       res.status(401).json(err);
@@ -28,7 +28,7 @@ router.post('/signup', (req, res) => {
 });
 
 // Route for getting some data about our user to be used client side
-router.get('/user_data', (req, res) => {
+router.get("/user_data", (req, res) => {
   if (!req.user) {
     // The user is not logged in, send back an empty object
     return res.json({});
@@ -39,7 +39,7 @@ router.get('/user_data', (req, res) => {
   res.json(user);
 });
 // matt added this API Route
-router.get('/members', isAuthenticated, (req, res) => {
+router.get("/members", isAuthenticated, (req, res) => {
   db.open_pos
     .findAll({})
     .then((results) => {
@@ -49,5 +49,18 @@ router.get('/members', isAuthenticated, (req, res) => {
       console.log(err);
     });
 });
+
+// I think this API route should work to update information but we need to add buttons on the front end
+// router.put("/api/members/:id", (req, res) => {
+//   db.open_pos
+//     .update(req.body, {
+//       where: {
+//         id: req.body.id,
+//       },
+//     })
+//     .then(function(trackingNumber) {
+//       res.json(trackingNumber);
+//     });
+// });
 
 module.exports = router;
